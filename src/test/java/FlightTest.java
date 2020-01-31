cf798ad69f20efd5c90031b9bca2a61df231924c
Gregor Lennie <gregorlennie@hotmail.co.uk>
2020-01-31 14:19:45 +0000

    Adds initial JUnit tests for Flight class properties

--- src/test/java/FlightTest.java
+++ src/test/java/FlightTest.java
@@ -1,2 +1,50 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class FlightTest {
+
+    private Flight flight;
+
+    @Before
+    public void before(){
+        flight = new Flight();
+    }
+
+    @Test
+    public void hasAPlane(){
+        assertEquals(plane, flight.getPlane());
+    }
+
+    @Test
+    public void hasAFlightNumber(){
+        assertEquals("EK028", flight.getFlightNumber());
+    }
+
+    @Test
+    public void startsWithNoPassengers(){
+        assertEquals(0, flight.countPassengers());
+    }
+
+    @Test
+    public void hasADestination(){
+        assertEquals(Airport.DXB, flight.getDestination());
+    }
+
+    @Test
+    public void canGetDestinationName(){
+        assertEquals("Dubai", flight.getDestinationNameFromEnum());
+    }
+
+    @Test
+    public void hasADepartureAirport(){
+        assertEquals(Airport.GLA, flight.getDepartureAirport());
+    }
+
+    @Test
+    public void canGetDepartureAirportName(){
+        assertEquals("Glasgow", flight.getDepartureAirportNameFromEnum());
+    }
+
 }
