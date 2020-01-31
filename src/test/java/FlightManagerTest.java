f0e01e0f1d93a31906ea8a18582e10f1b75c763f
Gregor Lennie <gregorlennie@hotmail.co.uk>
2020-01-31 15:20:38 +0000

    Adds initial JUnit test for FlightManager class

---
+++ src/test/java/FlightManagerTest.java
@@ -0,0 +1,25 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class FlightManagerTest {
+
+    private FlightManager flightManager;
+    private Flight flight;
+    private Plane plane;
+    private Passenger passenger1;
+
+    @Before
+    public void before(){
+        plane = new Plane(PlaneType.CESSNA);
+        passenger1 = new Passenger("Bobby Blue", 2);
+        flight = new Flight("AB123", Airport.EDI, Airport.GLA, plane, "08:00 01JAN2020")
+        flightManager = new FlightManager(flight);
+    }
+
+    @Test
+    public void hasFlightToManage(){
+        assertEquals(flight, flightManager.getFlight());
+    }
+}
