b8c6d41366c9fcf755423f4720351c2ab101517d
Gregor Lennie <gregorlennie@hotmail.co.uk>
2020-01-31 13:49:58 +0000

    Adds JUnit tests for Plane class

--- src/test/java/PlaneTest.java
+++ src/test/java/PlaneTest.java
@@ -1,2 +1,24 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class PlaneTest {
+
+    private Plane plane;
+
+    @Before
+    public void before(){
+        plane = new Plane();
+    }
+
+    @Test
+    public void canGetPlaneCapacity(){
+        assertEquals(5, plane.getCapacity());
+    }
+
+    @Test
+    public void canGetPlaneWeight(){
+        assertEquals(200, plane.getWeight());
+    }
 }
