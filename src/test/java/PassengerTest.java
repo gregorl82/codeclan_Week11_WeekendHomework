c9ad667e2dd1a67b91524614ccfb63de79326e4e
Gregor Lennie <gregorlennie@hotmail.co.uk>
2020-01-31 13:45:33 +0000

    Adds constructor and getters for Passenger class, tested with JUnit

--- src/test/java/PassengerTest.java
+++ src/test/java/PassengerTest.java
@@ -1,2 +1,24 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class PassengerTest {
+
+    private Passenger passenger;
+
+    @Before
+    public void before(){
+        passenger = new Passenger("Roger Red", 2);
+    }
+
+    @Test
+    public void canGetPassengerName(){
+        assertEquals("Roger Red", passenger.getName());
+    }
+
+    @Test
+    public void canGetNumberOfBags(){
+        assertEquals(2, passenger.getNumberOfBags());
+    }
 }
